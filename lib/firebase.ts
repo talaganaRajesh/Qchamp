1facd037154746eed1a6c7257d73afe9bba39204
Rajesh <talaganarajesh@gmail.com>
2025-06-14 15:34:56 +0530

    few fixes

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore"
 import { getFunctions } from "firebase/functions"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  apiKey: "AIzaSyCMY0Lw1Fz_PvNMTEc0uVQzTIVDWYjf2hg",
+  authDomain: "quizclash-4f23e.firebaseapp.com",
+  projectId: "quizclash-4f23e",
+  storageBucket: "quizclash-4f23e.firebasestorage.app",
+  messagingSenderId: "719534701098",
+  appId: "1:719534701098:web:6cc94546ec4ca211a6e153",
+  databaseURL: "https://quizclash-4f23e-default-rtdb.firebaseio.com/",
 }
 
 const app = initializeApp(firebaseConfig)
